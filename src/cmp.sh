0aceefe737683cc477e3c5953c0c20466b3de152
abram <abram@mill.(none)>
2013-09-28 22:16:11 -0400

    added some more ignore files.  compiles -O3

--- src/cmp.sh
+++ src/cmp.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-#CPP_FILES="gbl2ngc.cpp gbl2ngc_debug.cpp gbl2ngc_export.cpp gbl2ngc_construct.cpp gbl2ngc_offset.cpp gbl2ngc_aperture.cpp gbl2ngc_globals.cpp"
-CPP_FILES="gbl2ngc_offset.cpp gbl2ngc.cpp gbl2ngc_debug.cpp gbl2ngc_export.cpp gbl2ngc_construct.cpp gbl2ngc_aperture.cpp gbl2ngc_globals.cpp"
-#gcc -O3 gerber_interpreter.c -c 
-#gcc -O3 test_gerber_interpreter.c gerber_interpreter.o -o test_gerber_interpreter
-#g++ -O3 -frounding-math gerber_interpreter.o $CPP_FILES -lstdc++ -lgmp -lCGAL -lCGAL_Core -lmpfr -lboost_thread -o gbl2ngc
+CPP_FILES="gbl2ngc.cpp gbl2ngc_debug.cpp gbl2ngc_export.cpp gbl2ngc_construct.cpp gbl2ngc_offset.cpp gbl2ngc_aperture.cpp gbl2ngc_globals.cpp"
+
+gcc -O3 gerber_interpreter.c -c 
+gcc -O3 test_gerber_interpreter.c gerber_interpreter.o -o test_gerber_interpreter
+g++ -O3 -frounding-math gerber_interpreter.o $CPP_FILES -lstdc++ -lgmp -lCGAL -lCGAL_Core -lmpfr -lboost_thread -o gbl2ngc
 
 #gcc -O2 gerber_interpreter.c -c 
 #gcc -O2 test_gerber_interpreter.c gerber_interpreter.o -o test_gerber_interpreter
 #g++ -O2 -frounding-math gerber_interpreter.o $CPP_FILES -lstdc++ -lgmp -lCGAL -lCGAL_Core -lmpfr -lboost_thread -o gbl2ngc
 
-gcc -g gerber_interpreter.c -c 
-gcc -g test_gerber_interpreter.c gerber_interpreter.o -o test_gerber_interpreter
-g++ -g -frounding-math gerber_interpreter.o $CPP_FILES -lstdc++ -lgmp -lCGAL -lCGAL_Core -lmpfr -lboost_thread -o gbl2ngc
+#gcc -g gerber_interpreter.c -c 
+#gcc -g test_gerber_interpreter.c gerber_interpreter.o -o test_gerber_interpreter
+#g++ -g -frounding-math gerber_interpreter.o $CPP_FILES -lstdc++ -lgmp -lCGAL -lCGAL_Core -lmpfr -lboost_thread -o gbl2ngc
 
 #gcc gerber_interpreter.c -c 
 #gcc test_gerber_interpreter.c gerber_interpreter.o -o test_gerber_interpreter
