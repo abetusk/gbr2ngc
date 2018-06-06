fe45e22ec69c7ea48e0584049d0862034fc955dd
Drew Meier <darius.montez@gmail.com>
2018-06-06 16:22:58 -0400

    Implement rapid() and cut() shortcuts for easy G-code generation
    Usage:
    
    to move X3 and Y-5,
    `rapid(output_file, "xz", 3, -5);`
    
    Human-readable output will automatically be used
    if `gHumanReadable` is set.
    
    Signed-off-by: Drew Meier <darius.montez@gmail.com>

--- src/gbl2ngc_export.cpp
+++ src/gbl2ngc_export.cpp
@@ -20,6 +20,10 @@
 
 #include "gbl2ngc.hpp"
 
+// How many digits after the decimal point?
+// Yes, it should be a string :)
+#define GCODE_LENGTH_PRECISION "6"  
+
 static double unit_mm2in(double v) {
   return v/25.4;
 }
@@ -31,6 +35,50 @@ static double unit_in2mm(double v) {
 static double unit_identity(double v) {
   return v;
 }
+
+// Export a rapid (G0) movement to file.
+// `axes` is a string containing the letters of the axes to move.
+// Up to three axis coordinates can be provided.
+// Ex: rapid(f, "xz", 3, -5) will move X3 and Y-5
+void rapid(FILE* file, const char* axes, double one, double two=0, double three=0) {
+  int i;
+  double coords[] = {one, two, three};
+  
+  if (gHumanReadable) {
+    fprintf(file, "g0");  
+    for (i = 0; axes[i] != '\0'; i++) {
+      fprintf(file, " %c%." GCODE_LENGTH_PRECISION "f", tolower(axes[i]), coords[i]);
+    }  
+  } else {
+    fprintf(file, "G00");
+    for (i = 0; axes[i] != '\0'; i++) {
+      fprintf(file, "%c%." GCODE_LENGTH_PRECISION "f", toupper(axes[i]), coords[i]);
+    }
+  }
+
+  fprintf(file, "\n");
+}
+
+// G1 equiv. of rapid()
+void cut(FILE* file, const char* axes, double one, double two=0, double three=0) {
+  int i;
+  double coords[] = {one, two, three};
+  
+  if (gHumanReadable) {
+    fprintf(file, "g1");  
+    for (i = 0; axes[i] != '\0'; i++) {
+      fprintf(file, " %c%." GCODE_LENGTH_PRECISION "f", tolower(axes[i]), coords[i]);
+    }  
+  } else {
+    fprintf(file, "G01");
+    for (i = 0; axes[i] != '\0'; i++) {
+      fprintf(file, "%c%." GCODE_LENGTH_PRECISION "f", toupper(axes[i]), coords[i]);
+    }
+  }
+
+  fprintf(file, "\n");
+}
+
 
 void export_paths_to_gcode_unit( FILE *ofp, Paths &paths, int src_units_0in_1mm, int dst_units_0in_1mm)
 {
@@ -48,13 +96,15 @@ void export_paths_to_gcode_unit( FILE *ofp, Paths &paths, int src_units_0in_1mm,
     }
   }
 
+  //fprintf(ofp, "%s\n", gBlockHeader);
+
   if (gHumanReadable) {
     fprintf(ofp, "f%i\n", gFeedRate);
-    fprintf(ofp, "g1 z%f", gZSafe);
   } else {
     fprintf(ofp, "F%i\n", gFeedRate);
-    fprintf(ofp, "G01Z%f\n", gZSafe);
   }
+
+  rapid(ofp, "z", gZSafe);
 
   if (gHumanReadable) {
     fprintf(ofp, "\n");
@@ -80,47 +130,35 @@ void export_paths_to_gcode_unit( FILE *ofp, Paths &paths, int src_units_0in_1mm,
     m = paths[i].size();
     for (j=0; j<m; j++)
     {
+      double x = f(ctod( paths[i][j].X ));
+      double y = f(ctod( paths[i][j].Y ));
+
       if (first)
       {
-
-        if (gHumanReadable) {
-          fprintf(ofp, "g0 x%f y%f\n", f(ctod( paths[i][j].X )), f(ctod( paths[i][j].Y )) );
-          fprintf(ofp, "g1 z%f\n", gZCut);
-        } else {
-          fprintf(ofp, "G00X%fY%f\n", f(ctod( paths[i][j].X )), f(ctod( paths[i][j].Y )) );
-          fprintf(ofp, "G01Z%f\n", gZCut);
-        }
+        rapid(ofp, "xy", x, y);
+        cut(ofp, "z", gZCut);
 
         first = 0;
       }
       else
       {
-
-        if (gHumanReadable) {
-          fprintf(ofp, "g1 x%f y%f\n", f(ctod( paths[i][j].X )), f(ctod( paths[i][j].Y )) );
-        } else {
-          fprintf(ofp, "G01X%fY%f\n", f(ctod( paths[i][j].X )), f(ctod( paths[i][j].Y )) );
-        }
-
+        cut(ofp, "xy", x, y);
       }
-
     }
 
     // go back to start
     //
-    if (gHumanReadable) {
-      fprintf(ofp, "g1 x%f y%f\n", f(ctod( paths[i][0].X )), f(ctod( paths[i][0].Y )) );
-      fprintf(ofp, "g1 z%f\n", gZSafe);
-    } else {
-      fprintf(ofp, "G01X%fY%f\n", f(ctod( paths[i][0].X )), f(ctod( paths[i][0].Y )) );
-      fprintf(ofp, "G01Z%f\n", gZSafe);
-    }
-
+    double start_x = f(ctod( paths[i][0].X));
+    double start_y = f(ctod( paths[i][0].Y));
+    cut(ofp, "xy", start_x, start_y);
+    cut(ofp, "z", gZSafe);
   }
 
   if (gHumanReadable) {
     fprintf(ofp, "\n\n");
   }
+
+  //fprintf(ofp, "%s\n", gBlockFooter);
 
 }
 
